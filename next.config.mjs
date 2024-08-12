625730b7c7b605520f61a8e9d4c324f535057300
xlj-hly <3532818356@qq.com>
2024-08-12 17:54:13 +0800

    env

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        API_ADDRESS: "https://xlj-hly-next-learn-my-app.vercel.app/api"
+        API_ADDRESS: "https://xlj-hly-next-learn-my-app.vercel.app/"
     }
 };
 
